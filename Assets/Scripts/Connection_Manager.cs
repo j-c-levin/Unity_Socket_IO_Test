5caa38e8f51e92ad7774e61150d67d5e7116c1ac
joshua levin <joshua.c.levin@gmail.com>
2016-12-20 22:23:54 +0000

    WebGL page connects to a localhost socket.io server on load

---
+++ Assets/Scripts/Connection_Manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Connection_Manager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        ConnectToServer();
+    }
+    public void ConnectToServer()
+    {
+        Debug.Log("connecting");
+        Application.ExternalCall("Logon");
+    }
+
+    public void OnRoomJoined()
+    {
+
+    }
+
+    public void SpawnPlayer()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
